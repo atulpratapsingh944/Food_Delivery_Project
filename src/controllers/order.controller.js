99ef6b5e603d87c85dc34bfec75dbbf4c5976804
atulpratapsingh944 <198121030+atulpratapsingh944@users.noreply.github.com>
2025-05-23 14:05:07 +0530

    jkk

--- src/controllers/order.controller.js
+++ src/controllers/order.controller.js
@@ -1,61 +1,88 @@
-const orderCollection = require('../models/order.model');
-const userCollection = require('../models/user.model');
-let foodCollection = require('../models/food.model');
-const asyncHandler = require('express-async-handler');
+const orderCollection = require("../models/order.model");
+const userCollection = require("../models/user.model");
+const foodCollection = require("../models/food.model");
+const asyncHandler = require("express-async-handler");
+const { Stripe } = require("stripe");
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 const placeOrder = asyncHandler(async (req, res) => {
-    // ! Current Logged in user
-    const currentUser = req.myUser;
+  //! current logged in user
+  const currentUser = req.myUser;
+  //   console.log(currentUser);
+  //! get the cart details of current logged in user
+  const cartData = currentUser.cartData;
+  //! address of the current logged in user
+  const { address } = req.body;
+  //   console.log(req.body);
 
+  //! extracting the keys from cart data into an array
+  let foodIds = Object.keys(cartData);
+  //   console.log(foodIds);
+  //! using that array, finding all the food items
+  let foodItems = await foodCollection.find({ _id: { $in: foodIds } });
+  //   console.log(foodItems);
+  console.log(cartData);
+  console.log(foodItems);
 
-    // ! get the cart data of the current logged in user 
-    const cartData = currentUser.cartData;
+  let items = [];
+  let total = 0;
 
-    // ! address of the current logged in user
-    const {address} = req.body;
-
-
-    // ! cart data of the current logged in user
-    // console.log(currentUser.cartData);
-
-    // ! extracting food ids from the cart data into an Array
-    let foodIds = Object.keys(currentUser.cartData);
-    
-
-
-// ! using the food ids to get the food items from the food collection
-    let  foodItems = await foodCollection.find({_id: {$in: foodIds}});
-    // console.log(foodItems);
-    console.log(cartData);
-
-
-    let  items = [];
-    let total = 0;
-
-
-for (let foodItem of foodItems) {
+  for (let foodItem of foodItems) {
     // console.log(foodItem);
-    let quantity = cartData[foodItem._id]; 
+    let quantity = cartData[foodItem._id];
     let price = foodItem.price;
     let itemPrice = quantity * price;
 
-
     total += itemPrice;
 
-    total += price * quantity;
-    
     items.push({
-        food : foodItem.name,
-        quantity: quantity,
-        price: itemPrice,
-        // total,
+      food: foodItem.name,
+      quantity: quantity,
+      price: itemPrice,
+      //   total,
     });
-}
-console.log(items);
+    // console.log(items);
+  }
+  const newOrder = await orderCollection.create({
+    userId: currentUser._id,
+    items: items,
+    amount: total,
+    address: address,
+  });
+
+  const line_items = items.map((item) => {
+    return {
+      price_data: {
+        currency: "inr",
+        product_data: {
+          name: item.food,
+        },
+        unit_amount: item.price * 100,
+      },
+      quantity: item.quantity,
+    };
+  });
+
+  console.log(line_items);
+
+  const session = await stripe.checkout.sessions.create({
+    line_items,
+    payment_method_types: ["card"],
+    mode: "payment",
+    success_url: `http://localhost:9000/oreders/v1/verify-order?orderId=${newOrder._id}&success=true`,
+    cancel_url: `http://localhost:9000/oreders/v1/verify-order?orderId=${newOrder._id}&success=false`,
+  });
+
+  console.log(session);
+
+  res.status(200).json({
+    success: true,
+    message: "order placed successfully",
+    sessionId: session.id,
+    url: session.url,
+  });
 });
 
 module.exports = {
-    placeOrder
-
+  placeOrder,
 };
-    
--- src/models/order.model.js
+++ src/models/order.model.js
@@ -26,7 +26,7 @@ const orderSchema = new mongoose.Schema({
 
     status: {
         type: String,
-        enum: ['pending','processing', 'shipped', 'delivered', 'out-of-delivery'],
+        enum: ['pending','processing','delivered', 'out-of-delivery'],
         default: 'pending'
     },
 }, { timestamps: true });
